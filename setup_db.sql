06d7b629accc3507709614fc3b85186aa5090b70
Sai Vishnu <saivishnu725@gmail.com>
2025-11-05 10:54:06 +0000

    varchar -> varchar2

--- setup_db.sql
+++ setup_db.sql
@@ -5,12 +5,12 @@ USE registration_system;
 CREATE TABLE
     users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        first_name VARCHAR(100),
-        last_name VARCHAR(100),
-        username VARCHAR(100) UNIQUE,
+        first_name varchar2(100),
+        last_name varchar2(100),
+        username varchar2(100) UNIQUE,
         dob DATE,
-        email VARCHAR(150) UNIQUE,
+        email varchar2(150) UNIQUE,
         address TEXT,
-        profile_pic VARCHAR(255),
+        profile_pic varchar2(255),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
